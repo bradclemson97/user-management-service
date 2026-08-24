6850a59a8594611ee22433554ec869e30a6c4b5c
bradclemson97 <bradclemson97@gmail.com>
2026-08-24 12:12:55 +0100

    enhancements

--- target/generated-sources/annotations/com/example/usermanagementservice/mapper/UserDetailsMapperImpl.java
+++ target/generated-sources/annotations/com/example/usermanagementservice/mapper/UserDetailsMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.16 (Oracle Corporation)"
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.20.1 (Homebrew)"
 )
 @Component
 public class UserDetailsMapperImpl implements UserDetailsMapper {
--- target/generated-sources/annotations/com/example/usermanagementservice/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/example/usermanagementservice/mapper/UserMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.16 (Oracle Corporation)"
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.20.1 (Homebrew)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
